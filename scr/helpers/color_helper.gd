a3937347f7905d28a943e607348ca3ce61799731
Diaz <cantonival@gmail.com>
2025-07-17 13:49:03 +0200

    Add LIGHT_GREEN and LIGHT_RED color constants

--- scr/helpers/color_helper.gd
+++ scr/helpers/color_helper.gd
@@ -2,5 +2,8 @@ extends RefCounted
 class_name ColorHelper
 
 const LIGHT_BLUE: Color = Color(0.449, 0.689, 0.954, 1.0)
+const LIGHT_GREEN: Color = Color(0.329, 0.671, 0.353, 1.0)
+const LIGHT_RED: Color = Color(0.894, 0.361, 0.314, 1.0)
+
 const BLUE: Color = Color(0.204, 0.42, 0.698, 1.0)
 const RED: Color = Color(0.722, 0.235, 0.2, 1.0)
